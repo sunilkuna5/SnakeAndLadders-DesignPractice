8f48ea3fd1ff8f1731f19f765bd221390100375c
Sunil Kumar Reddy Kuna <sunil@getmyparking.com>
2021-01-10 20:15:50 +0530

    Fixed Issue

--- src/main/java/CommandLineInterface.java
+++ src/main/java/CommandLineInterface.java
@@ -1,20 +1,19 @@
 import Game.Game;
+import Game.GameComponents.*;
 import Game.GameComponents.Board.Board;
 import Game.GameComponents.Board.BoardImpl;
 import Game.Chance;
-import Game.GameComponents.Dice;
-import Game.GameComponents.DiceImpl;
-import Game.GameComponents.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommandLineAdapter {
+public class CommandLineInterface {
 
     Board board = new BoardImpl(100);
     Dice dice = new DiceImpl(6);
     List<Player> players = new ArrayList<>();
-    Game game = new Game(board,players,dice);
+    PlayerSelector playerSelector = new PlayerSelectorImpl(players);
+    Game game = new Game(board,players,dice,playerSelector);
     String format = "%s rolled a %d and moved from %d to %d";
 
     public void addSnake(String snake){
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args){
-        CommandLineAdapter commandLineAdapter = new CommandLineAdapter();
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
         Scanner scanner = new Scanner(System.in);
         int noOfSnakes = scanner.nextInt();
         scanner.nextLine();
         while (noOfSnakes-->0)
-            commandLineAdapter.addSnake(scanner.nextLine());
+            commandLineInterface.addSnake(scanner.nextLine());
         int noOfLadders = scanner.nextInt();
         scanner.nextLine();
         while (noOfLadders-->0)
-            commandLineAdapter.addLadder(scanner.nextLine());
+            commandLineInterface.addLadder(scanner.nextLine());
         int noOfPlayers = scanner.nextInt();
         scanner.nextLine();
         while (noOfPlayers-->0)
-            commandLineAdapter.addPlayer(scanner.nextLine());
-        commandLineAdapter.play();
+            commandLineInterface.addPlayer(scanner.nextLine());
+        commandLineInterface.play();
     }
 }
